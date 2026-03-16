2774155e9602edf17047a16a27beb4d1b62e034b
Engineer Extreme <callmemrengineer@gmail.com>
2026-03-16 08:25:53 +0530

    Add new WindowEngine
    To keep track of all open windows

--- src/WindowEngine.ts
+++ src/WindowEngine.ts
@@ -1,6 +1,32 @@
-const { windowManager } = require("node-window-manager");
-
-const window = windowManager.getActiveWindow();
+import { BaseObserver } from './BaseObserver';
+import { windowManager } from 'node-window-manager'
 
 windowManager.requestAccessibility();
-window.bringToTop();
+
+export interface WindowListener {
+    windowsChanged: (windows: Window[]) => any;
+}
+
+export class WindowEngine extends BaseObserver<WindowListener> {
+    windows: Window[];
+    constructor() {
+        super();
+        this.windows = [];
+    }
+
+    recompute() {
+        this.windows = windowManager.getWindows();
+        this.iterateListeners((cb) => cb.windowsChanged?.(this.windows));
+    }
+
+    init() {
+        windowManager.on('window-activated', () => {
+            this.recompute();
+        });
+        this.recompute();
+    }
+
+    getWindow(name: string) {
+        return this.windows.find((d) => d.label === name);
+    }
+}
